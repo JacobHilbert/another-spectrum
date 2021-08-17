819deea22f1ea93bd06c71ed07d9bf4517e5bdf7
JacobHilbert <jacob.hilbert.tree@gmail.com>
2021-08-17 10:48:38 -0500

    line plot of first two cepheids
    connected dots for delta cephei  and eta aquilae
    
    .tsv files were row reversed. fixed
    
    also fixed plot details like color an legend

--- code/introduction/graph.py
+++ code/introduction/graph.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 #plt.rcParams["figure.dpi"] = 200
 plt.rcParams["axes.labelsize"] = 17
+plt.rcParams["legend.fontsize"] = 14
 
 path_prefix = "./code/introduction/"
 
@@ -11,11 +12,11 @@ plt.figure(figsize=(10,4))
 
 X,Y = np.loadtxt(path_prefix+"eta_aquilae.tsv").T
 P = 7.176641 # days
-X = np.concatenate((X*P,(X+1)*P))
+X = np.concatenate((X*P,(X+1)*P,(X+2)*P))
 X -= X.min()
-Y = np.concatenate((Y,Y))
+Y = np.concatenate((Y,Y,Y))
 
-plt.scatter(X, Y ,color='k',marker='o',label=r"$\eta$ Aquilæ")
+plt.plot(X, Y ,'-ok',label=r"$\eta$ Aquilæ")
 plt.ylim([4.4,3.4])
 plt.xlabel("days")
 plt.ylabel("V magnitude")
@@ -23,16 +24,17 @@ plt.ylabel("V magnitude")
 
 X,Y = np.loadtxt(path_prefix+"delta_cephei.tsv").T
 P = 5.366249 # days
-X = np.concatenate((X*P,(X+1)*P,(X+2)*P))
+X = np.concatenate((X*P,(X+1)*P,(X+2)*P,(X+3)*P))
 X -= X.min()
-Y = np.concatenate((Y,Y,Y))
+Y = np.concatenate((Y,Y,Y,Y))
 
-plt.scatter(X, Y ,color='white',edgecolors='k',marker='v',label=r"$\delta$ Cephei")
+plt.plot(X,Y,marker="v",color='gray',markerfacecolor="white",label=r"$\delta$ Cephei")
 plt.ylim([4.4,3.4])
 plt.xlabel("days")
 plt.ylabel("V")
+plt.xlim([0,20])
 
-plt.legend()
+plt.legend(loc=4)
 plt.tight_layout()
 #plt.show()
 plt.savefig("./text/img/eta_aquilae_delta_cephei_light_curves.pdf")
